6fb6261594680fb39caa955e54ca1ce26925e6f3
sherly <sherly800320@gmail.com>
2021-09-05 15:28:01 +0800

    add restaurant

--- HolyShong/Controllers/StoreController.cs
+++ HolyShong/Controllers/StoreController.cs
@@ -13,5 +13,9 @@ namespace HolyShong.Controllers
         {
             return View();
         }
+        public ActionResult Restaurant()
+        {
+            return View();
+        }
     }
 }
