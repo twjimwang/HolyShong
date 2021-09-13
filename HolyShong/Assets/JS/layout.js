523f273cef359209f869eb04dd38cf398f7ac709
Jessy Lee <jessy@gmail.com>
2021-09-13 11:17:13 +0800

    edit customerLayout

--- HolyShong/Assets/JS/layout.js
+++ HolyShong/Assets/JS/layout.js
@@ -23,26 +23,52 @@ menuCheck.addEventListener('click', function () {
 // 序號modal 關閉新增優惠modal
 let discountAdd = document.querySelector('#discountAdd');
 let discountModal = document.querySelector('#discountModal');
-discountAdd.addEventListener('click', function(){
+discountAdd.addEventListener('click', function () {
     discountModal = document.querySelector('#discountModal');
     discountModal.classList.add('d-none');
 });
 
 let ONdiscount = document.querySelectorAll('#ONdiscountModal button');
-ONdiscount.forEach(button =>{
-    button.addEventListener('click', function(){
+ONdiscount.forEach(button => {
+    button.addEventListener('click', function () {
         discountModal.classList.remove('d-none');
     })
 });
 
 //時間地點Modal on Modal
 let changeDest = document.querySelector("#deliverModal .modal-body div:first-of-type button");
-changeDest.addEventListener('click', function(){
+changeDest.addEventListener('click', function () {
     changeDest.parentNode.parentElement.offsetParent.classList.add('opacity-0');
 });
 let destModalBtn = document.querySelectorAll('#destModal button');
 destModalBtn.forEach(button => {
-    button.addEventListener('click',function(){
+    button.addEventListener('click', function () {
         changeDest.parentNode.parentElement.offsetParent.classList.remove('opacity-0');
     });
+});
+
+//cartList 按鈕點擊關閉購物車
+let cartBtn = document.querySelector('.cartList .cart-content .modal-header button');
+cartBtn.addEventListener('click', function () {
+    let cartInput = document.querySelector('#cart-check');
+    cartInput.click();
+});
+
+// 下拉選單
+$('.circleIcon').on('click', function () {
+
+    if ($(".checkboxDiv").hasClass('d-flex')) {
+        $('.checkboxDiv').removeClass('d-flex');
+        $('.checkboxDiv').addClass('d-none')
+
+        $('.circleIcon').removeClass('fa-chevron-circle-down');
+        $('.circleIcon').addClass('fa-chevron-circle-up');
+
+    } else {
+        $('.checkboxDiv').addClass('d-flex');
+        $('.checkboxDiv').removeClass('d-none');
+
+        $('.circleIcon').removeClass('fa-chevron-circle-up');
+        $('.circleIcon').addClass('fa-chevron-circle-down');
+    }
 });
