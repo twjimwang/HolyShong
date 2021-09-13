109cda3e2f5ce0de42c4edcf69c4c833527eba60
Flash <albert821107@gmail.com>
2021-09-13 11:30:12 +0800

    add homepage

---
+++ HolyShong/HolyShong/Controllers/DeliverController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HolyShong.Controllers
+{
+    public class DeliverController : Controller
+    {
+        // GET: Deliver
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
