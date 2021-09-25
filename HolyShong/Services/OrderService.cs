cc16dd58833511449c0b9bba6604cc19f066fa63
Jessy Lee <jessy@gmail.com>
2021-09-26 01:11:11 +0800

    add service

--- HolyShong/Services/OrderService.cs
+++ HolyShong/Services/OrderService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using HolyShong.Repositories;
+using HolyShong.ViewModels;
+using HolyShong.Models.HolyShongModel;
 
 namespace HolyShong.Services
 {
@@ -12,6 +14,25 @@ namespace HolyShong.Services
         public OrderService()
         {
             _repo = new HolyShongRepository();
+        }
+
+        /// <summary>
+        /// 單筆外送訂單呈現
+        /// </summary>
+        /// <returns></returns>
+        public OrderDeliverViewModel GetOrder()
+        {
+            var orderId = 1;
+            OrderDeliverViewModel order = new OrderDeliverViewModel()
+            {
+                OderLists = new List<OrderList>()
+            };
+
+            //取得該訂單商品明細
+            order.OderLists = _repo.GetAll<Order>().Where();
+            
+
+            return null;
         }
 
     }
---
+++ HolyShong/ViewModels/OrderDeliverViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolyShong.ViewModels
+{
+    public class OrderDeliverViewModel
+    {
+        public string DeliverName { get; set; }
+        public string Address { get; set; }
+        public string Notes { get; set; }
+        public string RestaurantName { get; set; }
+        public List<OrderList> OderLists { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class OrderList
+    {
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal ProductTotal { get; set; }
+
+    }
+}
