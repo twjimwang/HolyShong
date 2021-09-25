318bca863a3f3255d32a252edae5de60d8386026
Jessy Lee <jessy@gmail.com>
2021-09-25 19:19:14 +0800

    readd model

---
+++ HolyShong/Models/HolyShongModel/Deliver.cs
@@ -0,0 +1,44 @@
+namespace HolyShong.Models.HolyShongModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Deliver")]
+    public partial class Deliver
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Deliver()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int DeliverId { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime? UpdateTime { get; set; }
+
+        public int MemberId { get; set; }
+
+        [Required]
+        public string IdentityAndLicenseImg { get; set; }
+
+        [Required]
+        public string InsuranceImg { get; set; }
+
+        [Required]
+        public string HeadshotImg { get; set; }
+
+        [Required]
+        public string PoliceCriminalRecordImg { get; set; }
+
+        public virtual Member Member { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
---
+++ HolyShong/Models/HolyShongModel/Store.cs
@@ -0,0 +1,74 @@
+namespace HolyShong.Models.HolyShongModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Store")]
+    public partial class Store
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Store()
+        {
+            Businesshours = new HashSet<Businesshours>();
+            Cart = new HashSet<Cart>();
+            Favorite = new HashSet<Favorite>();
+            ProductCategory = new HashSet<ProductCategory>();
+            Score = new HashSet<Score>();
+            DiscountStroe = new HashSet<DiscountStroe>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int StoreId { get; set; }
+
+        public int StoreCategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Img { get; set; }
+
+        public string KeyWord { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Cellphone { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime? UpdateTime { get; set; }
+
+        public bool IsDelete { get; set; }
+
+        public int? MemberId { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Businesshours> Businesshours { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cart> Cart { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Favorite> Favorite { get; set; }
+
+        public virtual Member Member { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ProductCategory> ProductCategory { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Score> Score { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<DiscountStroe> DiscountStroe { get; set; }
+
+        public virtual StoreCategory StoreCategory { get; set; }
+    }
+}
