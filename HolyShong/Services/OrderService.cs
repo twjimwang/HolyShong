4798d933da91d422f26c47397a9aaf6bda62f02f
Jessy Lee <jessy@gmail.com>
2021-09-26 00:25:48 +0800

    edit service

---
+++ HolyShong/Services/OrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HolyShong.Repositories;
+
+namespace HolyShong.Services
+{
+    public class OrderService
+    {
+        private readonly HolyShongRepository _repo;
+        public OrderService()
+        {
+            _repo = new HolyShongRepository();
+        }
+
+    }
+}
