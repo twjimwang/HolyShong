c5f90153d01e8fca74749e22b599e6198beb71df
sherly <sherly800320@gmail.com>
2021-09-28 18:01:49 +0800

    修改restaurant action

---
+++ HolyShong/Services/RestaurantService.cs
@@ -0,0 +1,18 @@
+﻿using HolyShong.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolyShong.Services
+{
+    public class RestaurantService
+    {
+        private readonly HolyShongRepository _repo;
+        public RestaurantService()
+        {
+            _repo = new HolyShongRepository();
+        }
+        
+    }
+}
