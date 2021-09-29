4fb6cca840c0067d80affd8af0cb22edd457bf24
DINGDING <DINGDING>
2021-09-29 19:27:09 +0800

    test

---
+++ HolyShong/Services/CartService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HolyShong.ViewModels;
+using HolyShong.Repositories;
+
+namespace HolyShong.Services
+{
+    public class CartService
+    {
+        private readonly HolyShongRepository _repo;
+        public CartService()
+        {
+            _repo = new HolyShongRepository();
+        }
+
+        public CartViewModel GetCartViewModel()
+        {
+            var result = new CartViewModel()
+            {
+                Items = new List<Item>()
+                {
+                    new Item (){ProductOptionCards = new List<ProductOptionCard>() }
+                }   
+            };
+            return result;
+        }
+
+
+    }
+     
+}
