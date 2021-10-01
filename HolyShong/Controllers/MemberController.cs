b0e13393866459824cfcd3f8659286526494e46a
Leo115 <dophin115@gmail.com>
2021-10-01 22:38:06 +0800

    upData修改MemberC

--- HolyShong/Controllers/MemberController.cs
+++ HolyShong/Controllers/MemberController.cs
@@ -111,5 +111,75 @@ namespace HolyShong.Controllers
         {
             return View();
         }
+
+        /// <summary>
+        /// 登入頁面
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(MemberLoginViewModel loginVM)
+        {
+            //一.未通過驗證
+            if (!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            //二.通過Model驗證後, 使用HtmlEncode將帳密做HTML編碼, 去除有害的字元
+            string name = HttpUtility.HtmlEncode(loginVM.Email);
+            string password = HttpUtility.HtmlEncode(loginVM.Password);
+            //string password = HashService.MD5Hash(HttpUtility.HtmlEncode(loginVM.Password));
+
+            //三.EF比對資料庫帳密
+            //以Name及Password查詢比對Account資料表記錄
+
+            Member user = _memberLoginService.UserLogin(loginVM);
+            //Account user = _ctx.Accounts.Where(x => x.Name.ToUpper() == name.ToUpper() && x.Password == password).FirstOrDefault();
+
+            //Account user2 = _ctx.Accounts.SingleOrDefault(x => x.Name.ToUpper() == name.ToUpper() && x.Password == password);
+            //找不到則彈回Login頁
+            if (user == null)
+            {
+                ModelState.AddModelError("Password", "無效的帳號或密碼!");
+
+                return View(loginVM);
+            }
+
+            //四.FormsAuthentication Class -- https://docs.microsoft.com/zh-tw/dotnet/api/system.web.security.formsauthentication?view=netframework-4.8
+
+            //FormsAuthenticationTicket Class
+            //https://docs.microsoft.com/zh-tw/dotnet/api/system.web.security.formsauthenticationticket?view=netframework-4.8
+
+
+            //1.建立FormsAuthenticationTicket
+            var ticket = new FormsAuthenticationTicket(
+            version: 1,
+            name: user.Email.ToString(), //可以放使用者Id
+            issueDate: DateTime.UtcNow,//現在UTC時間
+            expiration: DateTime.UtcNow.AddMinutes(30),//Cookie有效時間=現在時間往後+30分鐘
+            isPersistent: loginVM.Remember,// 是否要記住我 true or false
+            userData: user.FirstName.ToString(), //可以放使用者角色名稱
+            cookiePath: FormsAuthentication.FormsCookiePath);
+
+            //2.加密Ticket
+            var encryptedTicket = FormsAuthentication.Encrypt(ticket);
+
+            //3.Create the cookie.
+            var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+            Response.Cookies.Add(cookie);
+
+            //4.取得original URL.
+            var url = FormsAuthentication.GetRedirectUrl(name, true);
+
+            //5.導向original URL
+            return Redirect(url);
+        }
     }
 }
