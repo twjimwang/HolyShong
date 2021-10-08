bfccb69d0a67b1a9191a4ed0ff30897a467b6f60
sherly <sherly800320@gmail.com>
2021-10-08 15:29:40 +0800

    test

--- HolyShong/ViewModels/RestaurantVM.cs
+++ HolyShong/ViewModels/RestaurantVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HolyShong.Models.HolyShongModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,30 @@ namespace HolyShong.ViewModels
 {
     public class RestaurantVM
     {
+        //ProductCategory
+        public List<ProductCategory> productCategories { get; set; }
+
+        public string SupplyTime { get; set; }
+
+        //Store
+        public int StoreId { get; set; }
+
+        public string StoreName { get; set; }
+
+        public string StorePicture { get; set; }
+
+        public string StoreAddress { get; set; }
+
+        //Product       
+        public List<Product> Products { get; set; }//需要很多商品,所以用成集合
+
+
+        public decimal? Score1 { get; set; }
+        //StoreCategory                     
+
+
+        public string StoreCategoryName { get; set; }
     }
+
+
 }
