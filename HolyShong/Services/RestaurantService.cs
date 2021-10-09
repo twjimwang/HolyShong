829edb84802cf95d68929d44a6777273d6cd7c7d
sherly <sherly800320@gmail.com>
2021-10-09 23:59:34 +0800

    test

--- HolyShong/Services/RestaurantService.cs
+++ HolyShong/Services/RestaurantService.cs
@@ -18,34 +18,29 @@ namespace HolyShong.Services
 
         public RestaurantVM GetRestaurant(int? id)
         {
-            //RestaurantVM result = new RestaurantVM()
-            //{
-            //    productCategories = new List<ProductCategory>(),
-            //    Products = new List<Product>()
-            //};
-            ProductAreaList result = new ProductAreaList()
+            RestaurantVM result = new RestaurantVM()
             {
-                ProductArea = new List<ProductArea>(),
-                Products = new List<Product>()
-            };
-            ProductArea result = new ProductArea()
-            {
-                Products = new List<ProductCard>()              
+                ProductAreaList = new List<ProductAreaList>()              
             };
 
+            //店家
             var store = _repo.GetAll<Store>().FirstOrDefault((x) => x.StoreId == id);
             if (store == null)
             {
                 return result;
             }
+            //產品類別(錨點)
             var productCategories = _repo.GetAll<ProductCategory>().Where((x) => x.StoreId == store.StoreId);
+            //產品
             var products = _repo.GetAll<Product>().Where(x => productCategories.Select(y => y.ProductCategoryId).Contains(x.ProductCategoryId));
+            var businessHours = _repo.GetAll<Businesshours>().Where(x => x.StoreId == store.StoreId);
             result.StoreId = store.StoreId;
             result.StoreName = store.Name;
             result.StorePicture = store.Img;
             result.StoreAddress = store.Address;
-            result.Products = products.ToList();
-            result.productCategories = productCategories.ToList();
+            //result.SupplyTime = businessHours;
+            //result.Products = products.ToList();
+            //result.productCategories = productCategories.ToList();
             result.Score1 = _repo.GetAll<Score>().Where(x => x.ScoreId == store.StoreId).Average(x => (decimal?)x.Score1) == null ? 0 : _repo.GetAll<Score>().Where(x => x.ScoreId == store.StoreId).Average(x => x.Score1);
 
             result.StoreCategoryName = _repo.GetAll<StoreCategory>().First(x => x.StoreCategoryId == store.StoreCategoryId).Name;
