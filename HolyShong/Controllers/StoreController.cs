75797ee0fc71e72cd9ae4ac95bd85fb2be68058c
sherly <sherly800320@gmail.com>
2021-10-18 16:41:51 +0800

    develop market修改

--- HolyShong/Controllers/StoreController.cs
+++ HolyShong/Controllers/StoreController.cs
@@ -10,9 +10,11 @@ namespace HolyShong.Controllers
     public class StoreController : Controller
     {
         private readonly ProductService _productService;
+        private readonly MarketService _marketService;
         public StoreController()
         {
-            _productService = new ProductService();     
+            _productService = new ProductService();
+            _marketService = new MarketService();
         }
         // GET: Store
         public ActionResult Index()
@@ -72,9 +74,18 @@ namespace HolyShong.Controllers
         //}
 
 
-        public ActionResult Marketing()
+        public ActionResult Marketing(int? id)
         {
-            return View();
+            if (!id.HasValue)
+            {
+                return RedirectToAction("NoSearch", "Home");
+            }
+            var result = _marketService.GetEventRestaurant(id.Value);
+            if (result.eventRestaurants == null)
+            {
+                return RedirectToAction("NoSearch", "Home");
+            }
+            return View(result);
         }
     }
 }
---
+++ HolyShong/Services/MarketService.cs
@@ -0,0 +1,75 @@
+﻿using HolyShong.Models.HolyShongModel;
+using HolyShong.Repositories;
+using HolyShong.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolyShong.Services
+{
+    public class MarketService
+    {
+        private readonly HolyShongRepository _repo;
+        public MarketService()
+        {
+            _repo = new HolyShongRepository();
+        }
+        public MarketViewModel GetEventRestaurant(int DiscountId)
+        {
+            var result = new MarketViewModel();
+            #region 一
+            ////找一個優惠
+            //var discount = _repo.GetAll<Discount>().FirstOrDefault(d => d.DiscountId == DiscountId);
+            //result.EventTitle = discount.Title;
+            //result.EventContent = discount.Contents;
+            //result.EventStart = (DateTime)discount.StartTime;
+            //result.EventEnd = (DateTime)discount.EndTime;
+
+            //result.eventRestaurants = new List<EventRestaurant>();
+
+            ////找一個優惠下面所有店家
+            //var discountStore = _repo.GetAll<DiscountStroe>().FirstOrDefault(ds => ds.DiscountId == discount.DiscountId);
+            //var store = _repo.GetAll<Store>().Where(s => s.StoreId == discountStore.StoreId);
+            //foreach(var s in store)
+            //{
+            //    var sTemp = new EventRestaurant()
+            //    {
+            //        RestaurantImg = s.Img,
+            //        RestaurantName = s.Name
+            //    };
+            //    result.eventRestaurants.Add(sTemp);
+            //}
+
+            #endregion
+
+            #region 二
+            //方法二
+            //找一個優惠
+            var discount = _repo.GetAll<Discount>().FirstOrDefault(d => d.DiscountId == DiscountId);
+            var discountStore = _repo.GetAll<DiscountStroe>().Where(ds=>ds.DiscountId==discount.DiscountId);
+            var store = _repo.GetAll<Store>().Where(s => discountStore.Select(ds => ds.StoreId).Contains(s.StoreId));
+
+            result.EventTitle = discount.Title;
+            result.EventContent = discount.Contents;
+            result.EventStart = (DateTime)discount.StartTime;
+            result.EventEnd = (DateTime)discount.EndTime;
+            
+            result.eventRestaurants = new List<EventRestaurant>();
+            //卡片區塊
+            foreach (var s in store)
+            {
+                var sTemp = new EventRestaurant()
+                {
+                    RestaurantImg = s.Img,
+                    RestaurantName = s.Name,
+                  
+                };
+                result.eventRestaurants.Add(sTemp);
+            }
+            #endregion
+
+            return result;
+        }
+    }
+}
