ff8557b5e5bc2e2575d9b40e343aed249e37def6
Leo115 <dophin115@gmail.com>
2021-10-19 19:10:23 +0800

    暫存檔案

--- HolyShong/Controllers/MemberController.cs
+++ HolyShong/Controllers/MemberController.cs
@@ -11,6 +11,7 @@ using HolyShong.Models.HolyShongModel;
 using System.Web.Security;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
 
 namespace HolyShong.Controllers
 {
@@ -20,12 +21,51 @@ namespace HolyShong.Controllers
         private readonly MemberProfileService _memberProfileService;
         private readonly OrderService _orderService;
 
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+
         public MemberController()
         {
             _memberService = new MemberService();
             _memberProfileService = new MemberProfileService();
             _orderService = new OrderService();
         }
+
+        public MemberController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            //_memberService = new MemberService();
+            //_memberProfileService = new MemberProfileService();
+            //_orderService = new OrderService();
+
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
 
 
         // GET: Member
@@ -136,45 +176,45 @@ namespace HolyShong.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult Register(MemberRegisterViewModel registerVM)
+        public async Task<ActionResult> Register(MemberRegisterViewModel registerVM)
         {
-            //if (ModelState.IsValid)
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = registerVM.Email, Email = registerVM.Email };
+                var result = await UserManager.CreateAsync(user, registerVM.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // 如需如何進行帳戶確認及密碼重設的詳細資訊，請前往 https://go.microsoft.com/fwlink/?LinkID=320771
+                    // 傳送包含此連結的電子郵件
+                    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    await UserManager.SendEmailAsync(user.Id, "確認您的帳戶", "請按一下此連結確認您的帳戶 <a href=\"" + callbackUrl + "\">這裏</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            // 如果執行到這裡，發生某項失敗，則重新顯示表單
+            return View(registerVM);
+
+            //if (!ModelState.IsValid)
             //{
-            //    var user = new ApplicationUser { UserName = registerVM.Email, Email = registerVM.Email };
-            //    var result = await UserManager.CreateAsync(user, registerVM.Password);
-            //    if (result.Succeeded)
-            //    {
-            //        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-            //        // 如需如何進行帳戶確認及密碼重設的詳細資訊，請前往 https://go.microsoft.com/fwlink/?LinkID=320771
-            //        // 傳送包含此連結的電子郵件
-            //        // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-            //        // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-            //        // await UserManager.SendEmailAsync(user.Id, "確認您的帳戶", "請按一下此連結確認您的帳戶 <a href=\"" + callbackUrl + "\">這裏</a>");
-
-            //        return RedirectToAction("Index", "Home");
-            //    }
-            //    AddErrors(registerVM);
+            //    return View(registerVM);
             //}
 
-            //// 如果執行到這裡，發生某項失敗，則重新顯示表單
-            //return View(registerVM);
+            //bool status = _memberService.CreateAccount(registerVM);
 
-            if (!ModelState.IsValid)
-            {
-                return View(registerVM);
-            }
-
-            bool status = _memberService.CreateAccount(registerVM);
-
-            if (status)
-            {
-                return Content("新增帳號成功,請收信進行驗證完成註冊!");
-            }
-            else
-            {
-                return Content("新增帳號失敗");
-            }
+            //if (status)
+            //{
+            //    return Content("新增帳號成功,請收信進行驗證完成註冊!");
+            //}
+            //else
+            //{
+            //    return Content("新增帳號失敗");
+            //}
 
         }
 
@@ -275,6 +315,14 @@ namespace HolyShong.Controllers
             //Response.Cookies.Add(new HttpCookie("ASP.NET_SessionId", ""));
 
             return RedirectToAction("Index", "Home");
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
         }
     }
 }
